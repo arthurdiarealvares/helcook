14e1d3bfb2e3222ecc31fa0aaa726ce7b504972c
arthurdiarealvares <arthur.diare@gmail.com>
2022-08-17 20:54:46 -0300

    initial commit

---
+++ src/main/kotlin/com/sphere/helpcook/HelpcookApplication.kt
@@ -0,0 +1,11 @@
+package com.sphere.helpcook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelpcookApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelpcookApplication>(*args)
+}
